de0980880fa0ba2a94bd35cba2880686b53daa9d
root <752087942@qq.com>
2019-12-05 23:51:33 +0800

    5

--- hello.py
+++ hello.py
@@ -1,10 +1,7 @@
 
 
 print("hello world!")
-<<<<<<< HEAD
 print(">>>>>>>>>>>>")
 print("I am xiaoyuhao!")
-=======
 print("I am xiaoyuhao!")
 print("<<<<<<<<<<<<<")
->>>>>>> 3b9d95bc57fb4f99b73bde596f7fee761b5556af
